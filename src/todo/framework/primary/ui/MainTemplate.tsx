f69b698973fe24d3eef59b5b533cf029cdc3bae9
Luis Benavides <luis.benavides@collectivehealth.com>
2024-03-06 19:12:23 -0300

    Changing add todo color

--- src/todo/framework/primary/ui/MainTemplate.tsx
+++ src/todo/framework/primary/ui/MainTemplate.tsx
@@ -23,7 +23,7 @@ export const MainTemplate = ({todos, error, showModal}: MainTemplateProps) => {
         <div className={"flex gap-3 grid-cols-3 w-screen px-20 my-5"}>
             {body}
             <Link
-                className={"bg-gradient-radial  from-teal-100 to-teal-50 absolute right-5 bottom-20 mb-5 pb-1 rounded-full  w-20 h-20 flex justify-center items-center  text-5xl"}
+                className={"bg-teal-400 absolute right-5 bottom-20 mb-5 pb-1 rounded-full  w-20 h-20 flex justify-center items-center  text-5xl"}
                 href={"/todos?modal=true"}>+</Link>
             {showModal && <NewTodoModal error={error}/>}
         </div>
