0e147856a2385197254f5aac91a2d35b6b8bb80b
Luis Benavides <luisalejobg@gmail.com>
2024-03-07 10:28:28 -0300

    removing some code

--- src/todo/framework/config/InstancesManager.ts
+++ src/todo/framework/config/InstancesManager.ts
@@ -1,9 +1,7 @@
 import {FetchAll, FetchAllUseCase} from "@/todo/application/fetchAll/FetchAll";
-import {InMemoryRepository} from "@/todo/framework/secondary/memory/InMemoryRepository";
 import {NewTodo, NewTodoUseCase} from "@/todo/application/newTodo/NewTodo";
 import {UpdateStatus, UpdateStatusUseCase} from "@/todo/application/updateStatus/UpdateStatus";
-import {Todo} from "@/todo/domain/Todo";
-import {createMongoInstance, MongoRepository} from "@/todo/framework/secondary/mongo/MongoRepository";
+import {MongoRepository} from "@/todo/framework/secondary/mongo/MongoRepository";
 import {FetchAllRepository} from "@/todo/application/fetchAll/FetchAllRepository";
 import {NewTodoRepository} from "@/todo/application/newTodo/NewTodoRepository";
 import {UpdateStatusRepository} from "@/todo/application/updateStatus/UpdateStatusRepository";
@@ -23,13 +21,6 @@ export const initApp = async () => {
         const fetchAllRepository: FetchAllRepository = mongoRepository;
         const newTodoRepository: NewTodoRepository = mongoRepository;
         const updateStatusRepository: UpdateStatusRepository = mongoRepository;
-
-        // await repository.store(Todo.of("Dummy 1", "Hello there 1"));
-        // await repository.store(Todo.of("Dummy 2", "Hello there 2").updateStatus());
-        // await repository.store(Todo.of("Dummy 3", "Hello there 3").updateStatus().updateStatus());
-        // await repository.store(Todo.of("Dummy 4", "Hello there 1"));
-        // await repository.store(Todo.of("Dummy 5", "Hello there 2").updateStatus());
-        // await repository.store(Todo.of("Dummy 6", "Hello there 3").updateStatus().updateStatus());
 
         fetchAll = new FetchAll(fetchAllRepository);
         newTodo = new NewTodo(newTodoRepository);
